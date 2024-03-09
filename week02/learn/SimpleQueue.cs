1cb69c295693102c6946409d6a8958c7134b33c6
graham ssendagire <ssendagiregraham745@gmail.com>
2024-03-09 18:41:28 +0300

    new wk2 assignments

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -11,6 +11,11 @@
         var value = queue.Dequeue();
         Console.WriteLine(value);
         // Defect(s) Found:
+        // Test 1: Enqueue one value and then Dequeue it.
+       //Expected Result: It should display 100.
+       // Defect(s) Found: 
+       //The Dequeue operation is removing the wrong index from the queue. 
+       //It should remove index 0 instead of index 1.
 
         Console.WriteLine("------------");
 
@@ -29,6 +34,7 @@
         value = queue.Dequeue();
         Console.WriteLine(value);
         // Defect(s) Found: 
+        //Similar to Test 1, the Dequeue operation is removing the wrong index from the queue.
 
         Console.WriteLine("------------");
 
@@ -45,6 +51,7 @@
             Console.WriteLine("I got the exception as expected.");
         }
         // Defect(s) Found: 
+        //The code does not raise an exception as expected when trying to dequeue from an empty queue. It should throw an IndexOutOfRangeException.
     }
 
     private readonly List<int> _queue = new();
@@ -54,7 +61,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -66,8 +73,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
